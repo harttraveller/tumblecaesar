1e7ee475df7f09f0f28165bc191539d4dfb964c8
Hart Traveller <hartktraveller@gmail.com>
2022-05-24 20:36:14 -0400

    change setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="tumblecaesar",
     version="0.0",
-    description="A module loosely based on the Caesar cipher, which enables the encoding/obfuscation of alphanumeric text.",
+    description="A module loosely based on the Caesar cipher, which enables the encoding/obfuscation of strings.",
     author="Hart Traveller",
     packages=find_packages(),
     include_package_data=True,
